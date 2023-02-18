d1046dbc76b721e1fbc4f84745e78ae4203be97d
Hung <hungamtp@gmail.com>
2023-02-18 11:31:51 +0700

    Init project

---
+++ src/test/java/hunnid/com/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package hunnid.com.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
